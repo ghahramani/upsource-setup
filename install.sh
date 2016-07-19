5744bd62362d1e60eb3fac71ec3f846175142b6b
navid <ghahramani.navid@gmail.com>
2016-07-19 13:19:13 +0300

    Fixed unzip problem and added running docker-compose too

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
-wget -o https://download.jetbrains.com/upsource/upsource-$1.zip
-unzip upsource-$1.zip -d upsource
-cd upsource
-mkdir docker
-cp -R ../docker ./docker
+CACHE=$2
+
+if [ "$CACHE" == "" ]; then
+	echo "Installing Upsource Version $1"
+	URL=https://download.jetbrains.com/upsource/upsource-$1.zip
+	echo "Downloading $URL"
+	wget "$URL"
+	CACHE=upsource-$1.zip
+fi
+
+FILENAME="${CACHE%.*}"
+echo "Unziping $CACHE"
+unzip $CACHE &&
+mv $FILENAME upsource &&
+cd upsource &&
+mkdir docker &&
+cp -R ../docker/ ./ &&
+cd docker &&
+docker-compose up -d &&
+docker-compose logs -f --tail=30
